9eb56b6cfe8e9db8b887e5e81f46f8784dd46b39
Adam Weil <adamjweil@gmail.com>
2019-08-26 02:15:44 -0400

    first commit

---
+++ routes/api/profile.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @ route GET api/profile/me
+// @desc Get current users profile
+// @access Public
+router.get("/me", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
