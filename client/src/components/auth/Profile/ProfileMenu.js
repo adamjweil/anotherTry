8fd550f44c2f8e29f2619d3bdb23000825b1dde9
meZ Profile <mezprofile@Adams-MacBook-Pro-2018.local>
2019-08-30 18:44:42 -0400

    updating small changes so I can change branh

--- client/src/components/auth/Profile/ProfileMenu.js
+++ client/src/components/auth/Profile/ProfileMenu.js
@@ -6,35 +6,34 @@ import { Link } from 'react-router-dom';
 
 const ProfileMenu = () => {
   return (
-    <Tabs
-      defaultTab="one"
-      onChange={(tabId) => { console.log(tabId) }}
-      >
-      <div>
+        <Tabs
+        defaultTab="one"
+        onChange={(tabId) => { console.log(tabId) }}
+        >
+        <div>
         <div className='ui pointing menu'>
           <TabList>
-            <Tab tabFor='one'>Messages</Tab>
-            <Tab tabFor='one'>Friends</Tab>
-            <Tab tabFor='one'>Tickets</Tab>
-          </TabList>
-          <div className='right menu'>
             <div className='item'>
-              <div className='ui icon input'>
-                <input type='text' placeholder='Search...' />
-                <i aria-hidden='true' className='search icon'></i>
-              </div>
+              <Tab tabFor='one'>Messages</Tab>
             </div>
+            <div className='item'>
+              <Tab tabFor='two'>Friends</Tab>
+            </div>
+            <div className='item'>
+              <Tab tabFor='three'>Tickets</Tab>
+            </div>
+          </TabList>
           </div>
-        </div>
-        </div>
+          </div>
+
         <TabPanel tabId='one'>
-          <p>Tab 1 content</p>
+          <p>Messages</p>
         </TabPanel>
          <TabPanel tabId='two'>
-          <p>Tab 2 content</p>
+          <p>Friends</p>
         </TabPanel>
         <TabPanel tabId='three'>
-          <p>Tab 3 content</p>
+          <p>Tickets</p>
         </TabPanel>
       </Tabs>
   );
