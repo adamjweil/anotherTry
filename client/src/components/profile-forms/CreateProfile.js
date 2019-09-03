eaa4920065ac928f70080db5a384b89b1a3de3b8
Adam Weil <adamjweil@gmail.com>
2019-09-03 03:14:57 -0400

    made the profile form nicer, still gettning the reducers to work for that

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,97 +1,155 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createProfile } from '../../actions/profile';
-import { Form, Button } from 'semantic-ui-react';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { createProfile } from "../../actions/profile";
+import { Form, Button, Grid, Column } from "semantic-ui-react";
+
+import SelectSkills from "./SelectSkills";
 
 const CreateProfile = ({ createProfile }) => {
   const [formData, setFormData] = useState({
-    team: '',
-    title: '',
-    bio: '',
-    reportingTo: '',
-    directReports: '',
-    hireDate: '',
+    team: "",
+    title: "",
+    bio: "",
+    reportingTo: "",
+    directReports: "",
+    hireDate: "",
     skills: [],
-    githubusername: '',
-    twitter: '',
-    linkedin: '',
-    facebook: '',
-    youtube: ''
+    githubusername: "",
+    twitter: "",
+    linkedin: "",
+    facebook: "",
+    youtube: ""
   });
 
-const { title, team, bio, reportingTo, directReports, hireDate, skills, githubusername, twitter, linkedin, facebook, youtube } = formData;
+  const {
+    title,
+    team,
+    bio,
+    reportingTo,
+    directReports,
+    hireDate,
+    skills,
+    githubusername,
+    twitter,
+    linkedin,
+    facebook,
+    youtube
+  } = formData;
 
-const onChange = (e) =>
-  setFormData({ ...formData, [e.target.name]: e.target.value })
-const onSubmit = (e) => {
-  e.preventDefault();
-  createProfile(formData)
-};
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const onProfileSaveClick = async e => {
+    e.preventDefault();
+    console.log({ ...formData });
+    createProfile({ ...formData });
+    return <Redirect to="/" />;
+  };
 
   return (
-    <div className='ui container'>
-      <Form size='large' onSubmit={e => onSubmit(e)}>
-        <select
-          name="team"
-          value={team}
-          onChange={e => onChange(e)}
-          className='ui dropdown'>
-          <option value="">Select Your team...</option>
-          <option value="Dev">Dev</option>
-          <option value="LogiQ">LogiQ</option>
-          <option value="Admin">Admin</option>
-        </select>
-          <br />
-        <select
-          name="title"
-          value={title}
-          onChange={e => onChange(e)}
-          className='ui dropdown'>
-          <option value="">Select Your title...</option>
-          <option value="Analyst">Analyst</option>
-          <option value="Associate">Associate</option>
-          <option value="Director">Director</option>
-          <option value="Senior Director">Senior Director</option>
-          <option value="Managing Director">Managing Director</option>
-        </select>
-          <br />
+    <div className="ui internally celled grid">
+      <div className="row">
+        <div className="three wide column">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Mezocliq_logo.png/240px-Mezocliq_logo.png" />
+        </div>
 
-        <textarea
-          placeholder="Enter a brief Bio here!"
-          rows="3"
-          name='bio'
-          value={bio}
-          onChange={e => onChange(e)}>
-        </textarea>
-          <br />
-          <br />
-        <textarea
-          placeholder="Enter your skills (comma seperated)"
-          rows="2"
-          name='skills'
-          value={skills}
-          onChange={e => onChange(e)}>
-        </textarea>
-          <br />
-          <br />
-        <Button color='blue' fluid size="small">
-          Save and Continue
-        </Button>
-        </Form>
+        <div class="ten wide column">
+          <div className="ui attached message">
+            <div className="header">Start Setting Your Profile Up Below!</div>
+            <p>
+              <i className="hand point left icon"></i>make sure to select Team &
+              Title!!
+            </p>
+          </div>
+
+          <Form className="ui form attached fluid segment">
+            <div className="field">
+              <label>Select your team:</label>
+              <select
+                name="team"
+                value={team}
+                onChange={e => onChange(e)}
+                className="ui dropdown"
+              >
+                <option value="">Team...</option>
+                <option value="Dev">Dev</option>
+                <option value="LogiQ">LogiQ</option>
+                <option value="Admin">Admin</option>
+                <option value="PM">PM</option>
+              </select>
+            </div>
+
+            <div className="field">
+              <label>Select your title:</label>
+              <select
+                name="title"
+                value={title}
+                onChange={e => onChange(e)}
+                className="ui dropdown"
+              >
+                <option value="">Title...</option>
+                <option value="Analyst">Analyst</option>
+                <option value="Associate">Associate</option>
+                <option value="Director">Director</option>
+                <option value="Senior Director">Senior Director</option>
+                <option value="Managing Director">Managing Director</option>
+              </select>
+            </div>
+            <div className="ui selection">
+              <label>Date of Hire:</label>
+              <input
+                type="date"
+                name="hireDate"
+                value={hireDate}
+                onChange={e => onChange(e)}
+              />
+            </div>
+            <br />
+            <div className="field">
+              <textarea
+                placeholder="Enter a brief Bio here!"
+                rows="3"
+                name="bio"
+                value={bio}
+                onChange={e => onChange(e)}
+              ></textarea>
+            </div>
+            <div className="field">
+              <label>Feel free to enter some of your skills below...</label>
+              <SelectSkills />
+            </div>
+            <div className="field">
+              <button
+                className="ui blue submit button"
+                onClick={onProfileSaveClick}
+                size="small"
+              >
+                Save
+              </button>
+            </div>
+          </Form>
+        </div>
+        <div class="three wide column">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Mezocliq_logo.png/240px-Mezocliq_logo.png" />
+        </div>
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
 CreateProfile.propTypes = {
   createProfile: PropTypes.func.isRequired,
-  users: PropTypes.array.isRequired
-}
+  users: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
   profile: state.profile,
   users: state.users
 });
 
-export default connect(mapStateToProps, { createProfile })(CreateProfile);
+export default connect(
+  mapStateToProps,
+  { createProfile }
+)(CreateProfile);
--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -1,62 +1,54 @@
 import {
   USER_LOADED,
-  AUTH_ERROR,
   GET_USERS,
   NOTIFCATION_INCREMENT,
   NOTIFCATION_DECREMENT,
-  GET_NOTIFICATION_COUNT } from "../actions/types";
+  GET_NOTIFICATION_COUNT
+} from "../actions/types";
 
-  const initialState = {
-    token: localStorage.getItem('token'),
-    firstName: '',
-    lastName: '',
-    isAuthenticated: null,
-    loading: true,
-    notification_count: 0,
-    user: null
-  };
+const initialState = {
+  token: localStorage.getItem("token"),
+  firstName: "",
+  lastName: "",
+  isAuthenticated: null,
+  loading: true,
+  notification_count: 0,
+  user: null
+};
 
-  export default function(state = initialState, action) {
-    const { type, payload } = action;
+export default function(state = initialState, action) {
+  const { type, payload } = action;
 
-    switch (type) {
-      case USER_LOADED:
-        return {
-          ...state,
-          isAuthenticated: true,
-          loading: false,
-          user: payload
-        }
-      case AUTH_ERROR:
-        localStorage.removeItem('token');
-        return {
-          ...state,
-          token: null,
-          isAuthenticated: false,
-          loading: false
-        }
-      case GET_USERS:
-        return  {
-          ...state,
-          isAuthenticated: true,
-          loading: false,
-          users: payload
-        };
-      case NOTIFCATION_INCREMENT:
-        state.notification_count = state.notification_count + 1
-        return {
-          ...state
-        }
-      case NOTIFCATION_DECREMENT:
-        state.notification_count = state.notification_count - 1
-        return {
-          ...state
-        }
-      case GET_NOTIFICATION_COUNT:
-        return {
-          ...state
-        }
-      default:
-        return state
-    }
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
+    case GET_USERS:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
+    case NOTIFCATION_INCREMENT:
+      state.notification_count = state.notification_count + 1;
+      return {
+        ...state
+      };
+    case NOTIFCATION_DECREMENT:
+      state.notification_count = state.notification_count - 1;
+      return {
+        ...state
+      };
+    case GET_NOTIFICATION_COUNT:
+      return {
+        ...state
+      };
+    default:
+      return state;
   }
+}
