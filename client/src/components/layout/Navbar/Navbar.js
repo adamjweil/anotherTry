adea2f6a768f75bdd3939e25d0ab1f8cf718c104
meZ Profile <mezprofile@Adams-MacBook-Pro-2018.local>
2019-09-13 14:10:01 -0400

    workng on directry

--- client/src/components/layout/Navbar/Navbar.js
+++ client/src/components/layout/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 import AuthenticatedNavbar from "./AuthenticatedNavbar";
@@ -17,7 +16,8 @@ const Navbar = ({ auth: { isAuthenticated, loading, isAdmin }, logout }) => {
 
 Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  isAdmin: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "./store";
-import "typeface-roboto";
 
 ReactDOM.render(
   <Provider store={store}>
