368c4e3e4804e2da3ff58f0f51de3ca1ebe20cbc
Adam Weil <adamjweil@gmail.com>
2019-09-16 22:41:18 -0400

    added the bio and skills section of the profile page

--- client/src/components/Profile/BasicInfoForm.js
+++ client/src/components/Profile/BasicInfoForm.js
@@ -2,19 +2,20 @@ import React, { Fragment, useState, useEffect } from "react";
 
 import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
+import { Typography, TextField, Button } from "@material-ui/core/";
+// import  from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { connect } from "react-redux";
 import { DatePicker, KeyboardDatePicker } from "@material-ui/pickers";
+import { createProfile } from "../../actions/profile";
 
-const BasicInfoForm = () => {
+const BasicInfoForm = ({ createProfile }) => {
   const [formData, setFormData] = useState(
     {
       firstName: "",
       lastName: "",
       handle: "",
-      hireDate: "",
+
       team: "",
       title: ""
     },
@@ -35,69 +36,71 @@ const BasicInfoForm = () => {
         Basic Information
       </Typography>
       <Grid container spacing={3}>
-        <form>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="firstName"
-              label="First Name"
-              value={firstName}
-              onChange={e => onChange(e)}
-              fullWidth
-              autoComplete="fname"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="lastName"
-              label="Last Name"
-              value={lastName}
-              onChange={e => onChange(e)}
-              fullWidth
-              autoComplete="lname"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="handle"
-              name="handle"
-              label="Handle"
-              value={handle}
-              onChange={e => onChange(e)}
-              fullWidth
-              autoComplete="handle"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <KeyboardDatePicker
-              variant="outlined"
-              label="Hire Date"
-              format="MM/dd/yyyy"
-              value={hireDate}
-              InputAdornmentProps={{ position: "start" }}
-              onChange={e => handleDateChange(e)}
-            />
-          </Grid>
-          <Grid item xs={1}></Grid>
-          <Grid item xs={10}>
-            <DatePicker
-              autoOk
-              orientation="landscape"
-              name="hireDate"
-              id="hireDate"
-              variant="static"
-              openTo="date"
-              value={hireDate}
-              onChange={e => handleDateChange(e)}
-            />
-          </Grid>
-        </form>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="firstName"
+            label="First Name"
+            value={firstName}
+            onChange={e => onChange(e)}
+            fullWidth
+            autoComplete="fname"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="lastName"
+            label="Last Name"
+            value={lastName}
+            onChange={e => onChange(e)}
+            fullWidth
+            autoComplete="lname"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id="handle"
+            name="handle"
+            label="Handle"
+            value={handle}
+            onChange={e => onChange(e)}
+            fullWidth
+            autoComplete="handle"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <KeyboardDatePicker
+            variant="outlined"
+            label="Hire Date"
+            format="MM/dd/yyyy"
+            value={hireDate}
+            InputAdornmentProps={{ position: "start" }}
+            onChange={e => handleDateChange(e)}
+          />
+        </Grid>
+        <Grid item xs={1}></Grid>
+        <Grid item xs={10}>
+          <DatePicker
+            autoOk
+            orientation="landscape"
+            name="hireDate"
+            id="hireDate"
+            variant="static"
+            openTo="date"
+            value={hireDate}
+            onChange={e => handleDateChange(e)}
+          />
+        </Grid>
+        <Button variant="contained" color="secondary" onClick={createProfile}>
+          Save
+        </Button>
       </Grid>
     </Fragment>
   );
 };
 
 BasicInfoForm.propTypes = {
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
+  createProfile: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -106,4 +109,7 @@ const mapStateToProps = state => ({
   user: state.auth.user
 });
 
-export default connect(mapStateToProps)(BasicInfoForm);
+export default connect(
+  mapStateToProps,
+  { createProfile }
+)(BasicInfoForm);
