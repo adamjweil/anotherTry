22caaf7704d41543ad751a7592c9564dd161c310
Adam Weil <adamjweil@gmail.com>
2019-09-23 10:35:50 -0400

    started over with the multistep form

--- client/src/components/Profile/BasicInfoForm.js
+++ client/src/components/Profile/BasicInfoForm.js
@@ -1,8 +1,10 @@
-import React, { Fragment, useState, useEffect } from "react";
-
+import React, { Fragment, useState, useEffect, Component } from "react";
+import AppBar from "material-ui/AppBar";
 import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
 import { Typography, TextField, Button } from "@material-ui/core/";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import RaisedButton from "material-ui/RaisedButton";
 // import  from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { connect } from "react-redux";
@@ -13,112 +15,90 @@ import {
   showErrorSnackbar,
   showSuccessSnackbar
 } from "../../actions/alert";
-const BasicInfoForm = ({ createProfile }) => {
-  const [formData, setFormData] = useState(
-    {
-      firstName: "",
-      lastName: "",
-      handle: ""
-    },
-    []
-  );
-  const { firstName, lastName, handle } = formData;
 
-  const [hireDate, handleDateChange] = useState({
-    hireDate: ""
-  });
-
-  // fetchUsers();
-
-  const onChange = e =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  const onProfileCreation = async e => {
+export class BasicInfoForm extends Component {
+  continue = e => {
     e.preventDefault();
-    try {
-      createProfile({ firstName, lastName, handle, hireDate });
-    } catch (err) {
-      showErrorSnackbar(err.msg);
-    }
+    this.props.nextStep();
   };
 
-  return (
-    <Fragment>
-      <Typography variant="h6" gutterBottom>
-        Basic Information
-      </Typography>
-      <form>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="firstName"
-              label="First Name"
-              value={firstName}
-              onChange={e => onChange(e)}
-              fullWidth
-              autoComplete="fname"
+  render() {
+    const { values, handleChange } = this.props;
+
+    return (
+      <MuiThemeProvider>
+        <Typography variant="h6" gutterBottom>
+          Basic Information
+        </Typography>
+        <Fragment>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="First Name"
+                hintText="Enter your First Name"
+                floatingLabelText="first Name"
+                onChange={handleChange("firstName")}
+                fullWidth
+                defaultValue={values.firstName}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Last Name"
+                hintText="Enter your Last Name"
+                floatingLabelText="last Name"
+                onChange={handleChange("lastName")}
+                defaultValue={values.lastName}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Handle"
+                hintText="Choose a Handle"
+                floatingLabelText="handle"
+                onChange={handleChange("handle")}
+                defaultValue={values.handle}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <KeyboardDatePicker
+                variant="outlined"
+                label="Hire Date"
+                format="MM/dd/yyyy"
+                InputAdornmentProps={{ position: "start" }}
+                onChange={handleChange("hireDate")}
+              />
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={10}>
+              <DatePicker
+                autoOk
+                orientation="landscape"
+                name="hireDate"
+                variant="static"
+                openTo="date"
+                onChange={handleChange("hireDate")}
+                defaultValue={values.handle}
+              />
+            </Grid>
+            <RaisedButton
+              label="Continue"
+              primary={true}
+              style={styles.button}
+              onClick={this.continue}
             />
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="lastName"
-              label="Last Name"
-              value={lastName}
-              onChange={e => onChange(e)}
-              fullWidth
-              autoComplete="lname"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="handle"
-              name="handle"
-              label="Handle"
-              value={handle}
-              onChange={e => onChange(e)}
-              fullWidth
-              autoComplete="handle"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <KeyboardDatePicker
-              variant="outlined"
-              label="Hire Date"
-              format="MM/dd/yyyy"
-              value={hireDate}
-              InputAdornmentProps={{ position: "start" }}
-              onChange={e => handleDateChange(e)}
-            />
-          </Grid>
-          <Grid item xs={1}></Grid>
-          <Grid item xs={10}>
-            <DatePicker
-              autoOk
-              orientation="landscape"
-              name="hireDate"
-              variant="static"
-              openTo="date"
-              value={hireDate}
-              onChange={e => handleDateChange(e)}
-            />
-          </Grid>
-          <Button
-            type="submit"
-            variant="contained"
-            color="secondary"
-            onClick={onProfileCreation}
-          >
-            Save
-          </Button>
-        </Grid>
-      </form>
-    </Fragment>
-  );
-};
+        </Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
 
 BasicInfoForm.propTypes = {
-  profile: PropTypes.object.isRequired,
-  createProfile: PropTypes.func.isRequired
+  // profile: PropTypes.object.isRequired,
+  // createProfile: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -127,7 +107,9 @@ const mapStateToProps = state => ({
   user: state.auth.user
 });
 
-export default connect(
-  mapStateToProps,
-  { createProfile }
-)(BasicInfoForm);
+const styles = {
+  button: {
+    margin: 15
+  }
+};
+export default BasicInfoForm;
