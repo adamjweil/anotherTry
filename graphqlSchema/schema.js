194e1306c93ac55362836c6ceddf35ad185c9bec
Adam Weil <adamjweil@gmail.com>
2019-11-04 02:43:13 -0500

    fjl

--- graphqlSchema/schema.js
+++ graphqlSchema/schema.js
@@ -14,22 +14,6 @@ const {
   GraphQLList
 } = graphql;
 
-const profiles = [
-  { _id: "1", firstName: "Adam", lastName: "Weil", teamId: "1" },
-  { _id: "2", firstName: "Bill", lastName: "Hen", teamId: "1" },
-  { _id: "3", firstName: "Will", lastName: "Chafe", teamId: "1" }
-];
-const users = [
-  { _id: "1", email: "adam@adam.weil" },
-  { _id: "2", email: "bill@bill.weil", profileId: "1" },
-  { _id: "3", email: "chi@chi.weil" }
-];
-const teams = [
-  { _id: "1", teamName: "Name 1", teamDescription: "descript 1" },
-  { _id: "2", teamName: "Name 2", teamDescription: "descript 2" },
-  { _id: "3", teamName: "Name 3", teamDescription: "descript 3" }
-];
-
 const ProfileType = new GraphQLObjectType({
   name: "Profile",
   fields: () => ({
@@ -37,17 +21,16 @@ const ProfileType = new GraphQLObjectType({
     user: {
       type: UserType,
       resolve(parent, args) {
-        console.log(parent);
-        return _.find(users, { _id: parent.userId });
+        // console.log(parent);
+        // return _.find(users, { _id: parent.user });
       }
     },
     team: {
       type: TeamType,
       resolve(parent, args) {
-        return _.find(teams, { _id: parent.teamId });
+        // return _.find(teams, { _id: parent.team });
       }
     },
-    profileId: { type: GraphQLID },
     firstName: { type: GraphQLString },
     lastName: { type: GraphQLString },
     team: { type: GraphQLString },
@@ -63,11 +46,9 @@ const UserType = new GraphQLObjectType({
     profile: {
       type: ProfileType,
       resolve(parent, args) {
-        console.log(parent);
-        return _.find(profiles, { _id: parent.profileId });
+        // return _.find(profiles, { _id: parent.profileId });
       }
     },
-    userId: { type: GraphQLID },
     email: { type: GraphQLString },
     terms: { type: GraphQLBoolean },
     password: { type: GraphQLString },
@@ -79,16 +60,14 @@ const TeamType = new GraphQLObjectType({
   name: "Team",
   fields: () => ({
     _id: { type: GraphQLID },
+    teamName: { type: GraphQLString },
+    teamDescription: { type: GraphQLString },
     profiles: {
       type: new GraphQLList(ProfileType),
       resolve(parent, args) {
-        return _.filter(profiles, { _id: parent.profiles });
+        // return _.filter(profiles, { teamId: parent._id });
       }
-    },
-
-    teamId: { type: GraphQLID },
-    teamName: { type: GraphQLString },
-    teamDescription: { type: GraphQLString }
+    }
   })
 });
 
@@ -100,7 +79,13 @@ const RootQuery = new GraphQLObjectType({
       args: { _id: { type: GraphQLID } },
       resolve(parent, args) {
         // Code to get data from db
-        return _.find(profiles, { _id: args._id });
+        // return _.find(profiles, { _id: args._id });
+      }
+    },
+    profiles: {
+      type: new GraphQLList(ProfileType),
+      resolve(parent, args) {
+        // return profiles;
       }
     },
     user: {
@@ -108,7 +93,13 @@ const RootQuery = new GraphQLObjectType({
       args: { _id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db
-        return _.find(users, { _id: args._id });
+        // return _.find(users, { _id: args._id });
+      }
+    },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parent, args) {
+        // return users;
       }
     },
     team: {
@@ -119,15 +110,43 @@ const RootQuery = new GraphQLObjectType({
         // return _.find(teams, { _id: args._id });
       }
     },
-    profiles: {
-      type: new GraphQLList(profiles),
+    teams: {
+      type: new GraphQLList(TeamType),
       resolve(parent, args) {
-        return profiles;
+        // return teams;
+      }
+    }
+  }
+});
+
+const Mutations = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addProfile: {
+      type: ProfileType,
+      args: {
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        team: { type: GraphQLString },
+        title: { type: GraphQLString },
+        bio: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        let profile = new Profile({
+          firstName: args.firstName,
+          lastName: args.lastName,
+          team: args.team,
+          title: args.titlt,
+          bio: args.bio
+        });
+        profile.save();
+        return args.firstName;
       }
     }
   }
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutations
 });
